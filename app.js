a57d6f28ba03686a18b2eb3e5f783e82498bae6b
Breck Cowan <breckcowan@gmail.com>
2022-05-31 09:44:28 -0500

    added nav bar functionality

--- app.js
+++ app.js
@@ -7,6 +7,16 @@
 const date = document.getElementById('date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    linksContainer.classList.toggle("show-links");
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+});
+
 
 // ********** fixed navbar ************
 
